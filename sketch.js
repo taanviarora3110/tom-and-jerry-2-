4810744ecb88ba9b1dd569cacebcd22b8ed65104
taanviarora3110 <78641755+taanviarora3110@users.noreply.github.com>
2021-03-17 23:34:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var tom,jerry;
+var tomImg;
+var jerryImg;
+
+function preload() {
+    //load the images here
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+var tom=createSprite(200,200,200,200);
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
